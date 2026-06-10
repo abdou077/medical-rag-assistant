873e0f27901453a0752f6ee8bda7eddaf1410f06
abdou077 <90404197+abdou077@users.noreply.github.com>
2026-06-10 19:44:25 +0200

    Implement FastAPI for Medical RAG Assistant

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rag import ask_question
 
+app = FastAPI(
+    title="Medical RAG Assistant",
+    description="Medical Question Answering using RAG",
+    version="1.0.0"
+)
+
+class Query(BaseModel):
+    question: str
+
+@app.get("/")
+def root():
+    return {
+        "message": "Medical RAG Assistant is running"
+    }
+
+@app.post("/ask")
+def ask(query: Query):
+
+    answer = ask_question(query.question)
+
+    return {
+        "question": query.question,
+        "answer": answer
+    }
